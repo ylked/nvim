935271b9c556f2a6d9109674ff2885cc99293ca9
Nima Dekhli <nima@dekhli.ch>
2024-10-08 21:24:10 +0200

    init config

---
+++ lua/plugins/community.lua
@@ -0,0 +1,9 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.recipes.vscode" },
+  -- example of importing a plugin
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  -- example of importing an entire language pack
+  -- these packs can set up things such as Treesitter, Language Servers, additional language specific plugins, and more!
+}
