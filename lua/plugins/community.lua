f3535b36329418ef3ae3ab7a2d3c850455cb4467
Nima Dekhli <nima@dekhli.ch>
2024-10-30 10:52:47 +0100

    change colorscheme

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -2,6 +2,7 @@ return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.recipes.vscode" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- example of importing a plugin
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   -- example of importing an entire language pack
