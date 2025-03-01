f2c82f6dd8b1a7c4c759c476dc51e1b363e85209
Nima Dekhli <nima@dekhli.ch>
2025-03-01 20:39:45 +0100

    update c compilation mappings

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -6,7 +6,23 @@ return {
         n = {
           ["<leader>xx"] = {"<cmd>echo 'test'<cr>", desc="test"},
           ["<leader>mp"] = {"<cmd>MarkdownPreview<cr>", desc="Preview markdown file"},
-          ["<leader>mb"] = {"<cmd>make run PROG=fnamemodify(expand('%'), ':r')", desc="Make run"}
+          ["<leader>mm"] = {"<cmd>!cd build && cmake -G Ninja ..<cr>"},
+          ["<F4>"] = {
+              function()
+                local filename = vim.fn.fnamemodify(vim.fn.expand('%:t'), ':r') 
+                local ninja_cmd = "ninja -C build " .. filename
+                
+                local result = vim.fn.system(ninja_cmd)
+              
+                if vim.v.shell_error == 0 then
+                  local run_cmd = "TermExec cmd='build/bin/" .. filename .. "'"
+                  vim.cmd(run_cmd)
+                else
+                  local run_cmd = "TermExec cmd='" .. ninja_cmd .. "'"
+                  vim.cmd(run_cmd)
+                end
+              end,
+            desc="Run with Ninja"}
         }
       }
     }
